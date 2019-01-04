91f9179af9277ab01aae8b5bdb8b11061b348d51
zhaoxilong <xilong.zhao@meari.com.cn>
2019-01-04 11:34:20 +0800

    add test code of delayqueue

---
+++ test/test_delayqueue.cc
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <unistd.h>
+#include "delayqueue.hh"
+
+
+
+
+using namespace std;
+
+
+
+
+class Handler: public DelayQueueEntry {
+public:
+    Handler(DelayTimeVal &delay) : DelayQueueEntry(delay) {
+        cout << "handler init finished" << endl;
+    }
+    ~Handler() {
+        cout << "deinit Handler" << endl;
+    };
+    virtual void handleTimeOut();
+};
+
+void Handler::handleTimeOut()
+{
+    DelayQueueEntry::handleTimeOut();
+}
+
+int main()
+{
+#if 1
+    int cnt = 0;
+    DelayQueue queue;
+    cout << "\n\n...\n\n" << endl;
+
+    DelayTimeVal val(8, 0);
+    val.PrintValue();
+    cout << "\n\n....\n\n" << endl;
+
+    Handler *entry = new Handler(val);
+    cout << "\n\n...\n\n" << endl;
+
+    queue.addEntry(entry);
+    cout << "\n\n...\n\n" << endl;
+
+    while (1) {
+        sleep(1);
+
+        queue.handleAlarm();
+
+        cout << "handlerAlarm finished, cnt = " << ++cnt << "\n\n\n\n" << endl;
+    }
+#endif
+    cout << "main function" << endl;
+}
