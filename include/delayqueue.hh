aa5a50a966309263de0e5627a503165bfb3f2bea
zhaoxilong <xilong.zhao@meari.com.cn>
2019-01-04 11:29:06 +0800

    fix class Delayqueue and related module

--- include/delayqueue.hh
+++ include/delayqueue.hh
@@ -1,49 +1,67 @@
 #ifndef _DELAY_QUEUE_HH
 #define _DELAY_QUEUE_HH
-#include "timevalue.hh"
-using namespace std;
+#include "delaytimeval.hh"
 
-class DelayTimeVal: public TimeVal {
-public:
-    DelayTimeVal(struct timeval *t)
-        :TimeVal(t)
-    { 
-        cout << "DelayTimeVal(struct timeval *t) init" << endl;
-    }
 
-    /** =, - need to deal **/
-    DelayTimeVal & operator=(const DelayTimeVal &delaytv);
 
-};
+
+extern const DelayTimeVal ZEROINTERVAl;
+extern const DelayTimeVal ETERNITY;
+class DelayQueue;
+
+
+
 
 /** abstract class, so constructor is protected for subclass invoking it **/
 class DelayQueueEntry {
-    friend class Delayqueue; 
+friend class DelayQueue;
 public:
     virtual ~DelayQueueEntry();//// must be virtual, so u can use baseclass deconstructor to call subclass deconstructor
     virtual void handleTimeOut();
 
 protected:
-    DelayQueueEntry(DelayTimeVal delay);
+    DelayQueueEntry(const DelayTimeVal &delay);
 
 private://// if defined protected, may only can access the Delayqueueentry of the Delayqueue, then others can not access
     DelayQueueEntry *pNext;
     DelayQueueEntry *pPrev;
-    DelayTimeVal delayDeltaTime;
+    DelayTimeVal delayDeltaTime; //// when DelayqueueEntry in DelayQueue, delatDeltaTime = ETERNITY, can not be changed
 };
 
+#if 1
 class DelayQueue: public DelayQueueEntry {
 public:
+    DelayQueue();
     void addEntry(DelayQueueEntry *newEntry);
     void removeEntry(DelayQueueEntry *newEntry);
     void handleAlarm();
+    DelayTimeVal const& timeToNextAlarm();
 
 private:
     void synchronizeTime();
-    DelayQueueEntry * head() { return pNext; }
+    DelayQueueEntry * head() const { return pNext; }
 
 private:
     DelayTimeVal lastSyncTime;
 
 };
+#else
+class DelayQueue {
+public:
+    DelayQueue();
+    void addEntry(DelayQueueEntry *newEntry);
+    void removeEntry(DelayQueueEntry *newEntry);
+    void handleAlarm();
+    DelayTimeVal const& timeToNextAlarm();
+
+private:
+    void synchronizeTime();
+    DelayQueueEntry * head() { return &entry; }
+
+private:
+    DelayTimeVal lastSyncTime;
+    DelayQueueEntry entry;
+
+};
+#endif
 #endif
--- src/delayqueue.cc
+++ src/delayqueue.cc
@@ -1,29 +1,41 @@
-#include "delayqueue.hh"
 #include <iostream>
+#include "delayqueue.hh"
 
 
 
 
-#define ZEROINTERVAl
+#define INT_MAX	0x7FFFFFFF
+#define MILLION 1000000
 using namespace std;
 
 
 
 
-/** DelayTimeVal **/
-/**need to test **/
-DelayTimeVal & operator=(const DelayTimeVal &delaytv)
+const DelayTimeVal ETERNITY(INT_MAX, MILLION);
+const DelayTimeVal ZEROINTERVAL(0, 0);
+
+
+
+
+DelayTimeVal TimeNow()
 {
-    TimeVal &tmp = delaytv;
-    *this = tmp;
-    return *this;
+    struct timeval val;
+    int ret = -1;
+
+    ret = gettimeofday(&val, NULL);
+    if (ret < 0) { //// maybe not correct
+        val.tv_sec = 0;
+        val.tv_usec = 0;
+    }
+
+    return DelayTimeVal(val);
 }
 
 
 
 
 /** DelayQueueEntry **/
-DelayQueueEntry::DelayQueueEntry(DelayTimeVal delay)
+DelayQueueEntry::DelayQueueEntry(const DelayTimeVal &delay)
     : delayDeltaTime(delay)
 {
     pNext = pPrev = this;
@@ -37,6 +49,7 @@ DelayQueueEntry::~DelayQueueEntry()
 
 void DelayQueueEntry::handleTimeOut()
 {
+    cout << "DelayQueueEntry::handleTimeOut()" << endl;
     delete this;//// ? delete what? virtual function, call subclass deconstructor
 }
 
@@ -44,6 +57,12 @@ void DelayQueueEntry::handleTimeOut()
 
 
 /** DelayQueue **/
+DelayQueue::DelayQueue(): DelayQueueEntry(ETERNITY) //// lastSyncTime default constructor
+{
+    cout << "DelayQueue() init\n\n" << endl;
+    lastSyncTime = TimeNow();
+}
+
 void DelayQueue::synchronizeTime()
 {
     struct timeval now;
@@ -52,22 +71,49 @@ void DelayQueue::synchronizeTime()
 
     gettimeofday(&now, NULL);//// ? need to deal return value
 
-    if (DelayTimeVal(&now) < lastSyncTime) {
-        lastSyncTime = DelayTimeVal(&now);
+    if (DelayTimeVal(now) < lastSyncTime) {
+        lastSyncTime = DelayTimeVal(now);
         return;
     }
 
-    syncInterval = DelayTimeVal(&now) - lastSyncTime;
-    lastSyncTime = DelayTimeVal(&now);
+    cout << "after DelayTimeVal(now) < lastSyncTime\n" << endl;
+
+    cout << "lastSyncTime: ";
+    lastSyncTime.PrintValue();
+
+    DelayTimeVal nowTimeVal(now);
+    cout << "timenow: ";
+    nowTimeVal.PrintValue();
+
+    syncInterval = (nowTimeVal - lastSyncTime);
+    lastSyncTime = nowTimeVal;
+
+    cout << "next == this " << (next == this) << endl;
+    ////cout << "next = " << next << ", this = " << this << endl;
+
+    cout << "syncInterval ->";
+    syncInterval.PrintValue();
+
+    cout << "lastSyncTime: ";
+    lastSyncTime.PrintValue();
+
+    cout << "next->delayDeltaTime ->";
+    next->delayDeltaTime.PrintValue();
 
     while ((next != this) && (syncInterval >= next->delayDeltaTime)) {
+        cout << "in while ((next != this) && (syncInterval >= next->delayDeltaTime))" << endl;
         syncInterval -= next->delayDeltaTime;
         next->delayDeltaTime = ZEROINTERVAL;
         next = next->pNext;
     }
 
+    cout << "after while ((next != this) && (syncInterval >= next->delayDeltaTime)) finished" << endl;
+
     if (next != this) {
+        cout << "next != this" << endl;
         next->delayDeltaTime -= syncInterval;
+    } else {
+        cout << "next == this\n" << endl;
     }
 
 }
@@ -82,20 +128,26 @@ void DelayQueue::addEntry(DelayQueueEntry *newEntry)
         return;
     }
    
-    synchronizeTime();
+    cout << "before synchronizeTime\n" << endl;
+    synchronizeTime();// cause newEnrty's delayDeltatime value is calculated from now
 
     /** add entry **/
     syncInterval = newEntry->delayDeltaTime;
+    syncInterval.PrintValue();
+    cout << "before while ((next != this) && (syncInterval >= next->delayDeltaTime))" << endl;
     while ((next != this) && (syncInterval >= next->delayDeltaTime)) {
         syncInterval -= next->delayDeltaTime;
         next = next->pNext;
     }
+
+    cout << "here in addEntry" << endl;
 
     if (next != this) {
         next->delayDeltaTime -= syncInterval;
         newEntry->delayDeltaTime = syncInterval;
     } else {
         newEntry->delayDeltaTime = syncInterval;
+        cout << "next == this" << endl;
     }
 
     next->pPrev->pNext = newEntry;
@@ -111,9 +163,26 @@ void DelayQueue::removeEntry(DelayQueueEntry *newEntry)
     if (!newEntry)
         return;
 
-    ////synchronizeTime();
+    ////synchronizeTime(); // dont't need synchronizeTime, cause all entries are all based on one lastSyncTime
+
+#if 0
+    if (newEntry == this)
+        return;
+#else // the following method is better
+    DelayQueueEntry *entry = this;
+    if (newEntry == entry) //// entry is delayQueue entry header
+        return;
+#endif
 
     /** need to check newEntry is in DelayQueue **/
+    entry = entry->pNext;
+    while (entry != this) {
+        if (entry == newEntry)
+            break;
+        entry = entry->pNext;
+    }
+    if (entry == this) //// newEntry not in delayQueue
+        return;
 
     if (newEntry->pNext != this) {
         newEntry->pNext->delayDeltaTime += newEntry->delayDeltaTime;
@@ -127,13 +196,14 @@ void DelayQueue::removeEntry(DelayQueueEntry *newEntry)
     return;
 }
 
- void DelayQueue::handleAlarm() //// process one event every time
+void DelayQueue::handleAlarm() //// process one event every time
 {
     if (head()->delayDeltaTime != ZEROINTERVAL) {
+        cout << "in if (head()->delayDeltaTime != ZEROINTERVAL)" << endl;
         synchronizeTime();
     }
 
-    if (head()->delayDeltaTime == ZEROINTERVAl) {
+    if (head()->delayDeltaTime == ZEROINTERVAL) {
         DelayQueueEntry *toRemove = head();
         removeEntry(toRemove);
         toRemove->handleTimeOut();
